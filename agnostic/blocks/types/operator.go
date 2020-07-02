fdc2a1ba48216ab503d2e62a165431664d158a0b
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-01 22:08:22 -0500

    feat(agnostic): add math and comparison operators

---
+++ agnostic/blocks/types/operator.go
@@ -0,0 +1,22 @@
+package types
+
+type MathOperator string
+
+const (
+	Add      MathOperator = "+"
+	Subtract MathOperator = "-"
+	Divide   MathOperator = "/"
+	Multiply MathOperator = "*"
+	Modulo   MathOperator = "%"
+)
+
+type ComparisonOperator string
+
+const (
+	Equal              ComparisonOperator = "=="
+	NotEqual           ComparisonOperator = "!="
+	GreatThan          ComparisonOperator = ">"
+	GreatThanOrEqualTo ComparisonOperator = ">="
+	LessThan           ComparisonOperator = "<"
+	LassThanOrEqualTo  ComparisonOperator = "<="
+)
