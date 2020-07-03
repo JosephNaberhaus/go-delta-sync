e39b065a2960f848005ba9e261a1c448945eff2d
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-03 18:32:10 -0500

    feat(agnostic): move operators to values package

---
+++ agnostic/blocks/values/operator.go
@@ -0,0 +1,21 @@
+package values
+
+type Operator string
+
+const (
+	Add                Operator = "+"
+	Subtract           Operator = "-"
+	Divide             Operator = "/"
+	Multiply           Operator = "*"
+	Modulo             Operator = "%"
+	Equal              Operator = "=="
+	NotEqual           Operator = "!="
+	GreatThan          Operator = ">"
+	GreatThanOrEqualTo Operator = ">="
+	LessThan           Operator = "<"
+	LassThanOrEqualTo  Operator = "<="
+)
+
+func (m Operator) Value() string {
+	return string(m)
+}
