aa3c02339a7c9fa431c0e6ed1431c6cda138ed20
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-06 21:32:07 -0500

    feat(agnostic): rename Map -> MapElement

--- agnostic/block/value/map.go
+++ agnostic/block/value/map.go
@@ -1,24 +1,24 @@
 package value
 
 // Refers to an element inside of a map
-type Map struct {
+type MapElement struct {
 	valueType
 	mapValue Any
 	key      Any
 }
 
 // THe map that contains the value
-func (m Map) Map() Any {
+func (m MapElement) Map() Any {
 	return m.mapValue
 }
 
 // The key of the element that is being referred to
-func (m Map) Key() Any {
+func (m MapElement) Key() Any {
 	return m.key
 }
 
-func NewMap(mapValue, key Any) Map {
-	return Map{
+func NewMap(mapValue, key Any) MapElement {
+	return MapElement{
 		mapValue: mapValue,
 		key:      key,
 	}
