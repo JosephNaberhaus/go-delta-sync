c4415d96cec6b17e143690594852d73d6a893c4a
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-06 21:37:31 -0500

    feat(agnostic): add create factory for test implementations

--- agnostic/targets/create.go
+++ agnostic/targets/create.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"github.com/JosephNaberhaus/go-delta-sync/agnostic"
 	"github.com/JosephNaberhaus/go-delta-sync/agnostic/targets/golang"
+	"github.com/JosephNaberhaus/go-delta-sync/agnostic/targets/test/generate"
 )
 
 func CreateImplementation(name string, args map[string]string) (implementation agnostic.Implementation, err error) {
@@ -12,4 +13,12 @@ func CreateImplementation(name string, args map[string]string) (implementation a
 	}
 
 	return nil, errors.New("No implementation found for \"" + name + "\"")
+}
+
+func CreateTestImplementation(name string, args map[string]string) (implementation generate.Implementation, err error) {
+	if name == "go" {
+		return golang.TestImplementation(args), nil
+	}
+
+	return nil, errors.New("No test implementation found for \"" + name + "\"")
 }
