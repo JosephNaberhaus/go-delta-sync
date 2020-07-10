5c415c90ea5fe172e91ba4d2c582f07de8d37f4b
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-09 23:03:37 -0500

    feat(agnostic): add typescript to create factory

--- agnostic/targets/create.go
+++ agnostic/targets/create.go
@@ -4,12 +4,15 @@ import (
 	"errors"
 	"github.com/JosephNaberhaus/go-delta-sync/agnostic"
 	"github.com/JosephNaberhaus/go-delta-sync/agnostic/targets/golang"
+	"github.com/JosephNaberhaus/go-delta-sync/agnostic/targets/typescript"
 	"github.com/JosephNaberhaus/go-delta-sync/agnostic/test"
 )
 
 func CreateImplementation(name string, args map[string]string) (implementation agnostic.Implementation, err error) {
 	if name == "go" {
 		return golang.NewImplementation(args), nil
+	} else if name == "typescript" {
+		return typescript.NewImplementation(args), nil
 	}
 
 	return nil, errors.New("No implementation found for \"" + name + "\"")
