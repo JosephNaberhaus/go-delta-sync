76ee02af52a8508b9fcf2a46403b0211df91a216
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-09 22:10:14 -0500

    feat(agnostic): add resolve value and types for typescript implementation

--- agnostic/targets/typescript/implementation.go
+++ agnostic/targets/typescript/implementation.go
@@ -3,8 +3,13 @@
 package typescript
 
 import (
+	"errors"
 	"fmt"
+	"github.com/JosephNaberhaus/go-delta-sync/agnostic/blocks/types"
+	"github.com/JosephNaberhaus/go-delta-sync/agnostic/blocks/value"
 	"io"
+	"strconv"
+	"strings"
 )
 
 const IndentAmount = 2
@@ -49,4 +54,97 @@ func (b BodyImplementation) Write(out io.Writer, indentLevel int) error {
 	}
 
 	return nil
+}
+
+func resolveType(any types.Any) string {
+	switch t := any.(type) {
+	case types.Base:
+		return resolveBaseType(t)
+	case types.Model:
+		return t.ModelName()
+	case types.Array:
+		return resolveType(t.Element()) + "[]"
+	case types.Map:
+		return "Map<" + resolveType(t.Key()) + ", " + resolveType(t.Value()) + ">"
+	case types.Pointer:
+		panic(errors.New("pointers are not supported yet"))
+	default:
+		panic(errors.New(fmt.Sprintf("unkown type %T", t)))
+	}
+}
+
+func resolveBaseType(base types.Base) string {
+	switch base {
+	case types.BaseBool:
+		return "boolean"
+	case types.BaseInt:
+		fallthrough
+	case types.BaseInt32:
+		fallthrough
+	case types.BaseInt64:
+		fallthrough
+	case types.BaseFloat32:
+		fallthrough
+	case types.BaseFloat64:
+		return "number"
+	case types.BaseString:
+		return "string"
+	default:
+		panic(errors.New("unknown base type " + string(base)))
+	}
+}
+
+func resolveValue(any value.Any) string {
+	switch v := any.(type) {
+	case value.Null:
+		return "null"
+	case value.String:
+		return "\"" + v.Value() + "\""
+	case value.Int:
+		return strconv.Itoa(v.Value())
+	case value.Float:
+		return strconv.FormatFloat(v.Value(), 'f', -1, 64)
+	case value.Array:
+		var sb strings.Builder
+
+		sb.WriteString(resolveType(v.ElementType()) + "[] {")
+		for i, element := range v.Elements() {
+			sb.WriteString(resolveValue(element))
+
+			if i-1 != len(v.Elements()) {
+				sb.WriteString(", ")
+			}
+		}
+		sb.WriteString("}")
+
+		return sb.String()
+	case value.Map:
+		var sb strings.Builder
+
+		sb.WriteString(resolveType(types.NewMap(v.KeyType(), v.ValueType())) + "([")
+		for i, element := range v.Elements() {
+			sb.WriteString("[" + resolveValue(element.Key()) + ", " + resolveValue(element.Value()) + "]")
+
+			if i-1 != len(v.Elements()) {
+				sb.WriteString(", ")
+			}
+		}
+		sb.WriteString("])")
+
+		return sb.String()
+	case value.OwnField:
+		return "this." + resolveValue(v.Field())
+	case value.Id:
+		return v.Name()
+	case value.ModelField:
+		return v.ModelName() + "." + resolveValue(v.Field())
+	case value.ArrayElement:
+		return resolveValue(v.Array()) + "[" + resolveValue(v.Index()) + "]"
+	case value.MapElement:
+		return resolveValue(v.Map()) + ".get(" + resolveValue(v.Key()) + ")"
+	case value.Combined:
+		return resolveValue(v.Left()) + " " + v.Operator().Value() + " " + resolveValue(v.Right())
+	default:
+		panic(errors.New(fmt.Sprintf("uknown type %T", v)))
+	}
 }
