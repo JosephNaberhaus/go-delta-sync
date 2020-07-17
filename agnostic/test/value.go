89be1380ec7f1f57985d1cd103b1340e6af79e58
Joseph Naberhaus <naberhausj@gmail.com>
2020-07-16 21:28:25 -0500

    feat(agnostic): change value tests to use string values

--- agnostic/test/value.go
+++ agnostic/test/value.go
@@ -6,29 +6,24 @@ import (
 	"github.com/JosephNaberhaus/go-delta-sync/agnostic/blocks/value"
 )
 
-// Test value types. Most of these tests are written with the intention of
-// catching syntax errors in generated code. Since the value generation code
-// is the same code that is used for test values, it is is difficult to
-// actually test that Agnostic is doing what is expected. To get around that,
-// arithmetic can be used to represent the same number in multiple ways.
 var ValueSuite = Suite{
 	{
 		Name:        "Int",
 		Description: "Support for integer types",
 		ModelFields: []agnostic.Field{
-			{Name: "Int1", Type: types.BaseInt},
-			{Name: "Int2", Type: types.BaseInt},
+			{Name: "String1", Type: types.BaseString},
+			{Name: "String2", Type: types.BaseString},
 		},
 		Generator: func(body agnostic.BodyImplementation) {
-			body.Assign(value.NewOwnField(value.NewId("Int1")), value.NewInt(-100))
-			body.Assign(value.NewOwnField(value.NewId("Int2")), value.NewInt(100))
+			body.Assign(value.NewOwnField(value.NewId("String1")), value.NewIntToString(value.NewInt(-100)))
+			body.Assign(value.NewOwnField(value.NewId("String2")), value.NewIntToString(value.NewInt(100)))
 		},
 		Facts: []Fact{
 			{
-				Name: "AssignsValues",
+				Name: "StringValueMatches",
 				SideEffects: []SideEffect{
-					{FieldName: "Int1", ExpectedValue: value.NewInt(-100)},
-					{FieldName: "Int2", ExpectedValue: value.NewInt(100)},
+					{FieldName: "String1", ExpectedValue: value.NewString("-100")},
+					{FieldName: "String2", ExpectedValue: value.NewString("100")},
 				},
 			},
 		},
@@ -37,19 +32,19 @@ var ValueSuite = Suite{
 		Name:        "IntPastMaxSafeFloatInteger",
 		Description: "Support for numbers outside of the range [-16777216, 16777216]",
 		ModelFields: []agnostic.Field{
-			{Name: "Int1", Type: types.BaseInt},
-			{Name: "Int2", Type: types.BaseInt},
+			{Name: "String1", Type: types.BaseString},
+			{Name: "String2", Type: types.BaseString},
 		},
 		Generator: func(body agnostic.BodyImplementation) {
-			body.Assign(value.NewOwnField(value.NewId("Int1")), value.NewInt(-16777216))
-			body.Assign(value.NewOwnField(value.NewId("Int2")), value.NewInt(16777216))
+			body.Assign(value.NewOwnField(value.NewId("String1")), value.NewIntToString(value.NewInt(-16777217)))
+			body.Assign(value.NewOwnField(value.NewId("String2")), value.NewIntToString(value.NewInt(16777217)))
 		},
 		Facts: []Fact{
 			{
-				Name: "AssignsValues",
+				Name: "StringValueMatches",
 				SideEffects: []SideEffect{
-					{FieldName: "Int1", ExpectedValue: value.NewCombined(value.NewInt(-16777217), value.Add, value.NewInt(1))},
-					{FieldName: "Int2", ExpectedValue: value.NewCombined(value.NewInt(16777217), value.Subtract, value.NewInt(1))},
+					{FieldName: "String1", ExpectedValue: value.NewString("-16777217")},
+					{FieldName: "String2", ExpectedValue: value.NewString("16777217")},
 				},
 			},
 		},
@@ -58,19 +53,19 @@ var ValueSuite = Suite{
 		Name:        "IntPastMaxSafeDoubleInteger",
 		Description: "Support for numbers outside of the range [-9007199254740992, 9007199254740992]",
 		ModelFields: []agnostic.Field{
-			{Name: "Int1", Type: types.BaseInt},
-			{Name: "Int2", Type: types.BaseInt},
+			{Name: "String1", Type: types.BaseString},
+			{Name: "String2", Type: types.BaseString},
 		},
 		Generator: func(body agnostic.BodyImplementation) {
-			body.Assign(value.NewOwnField(value.NewId("Int1")), value.NewInt(-9007199254740992))
-			body.Assign(value.NewOwnField(value.NewId("Int2")), value.NewInt(9007199254740992))
+			body.Assign(value.NewOwnField(value.NewId("String1")), value.NewIntToString(value.NewInt(-9007199254740993)))
+			body.Assign(value.NewOwnField(value.NewId("String2")), value.NewIntToString(value.NewInt(9007199254740993)))
 		},
 		Facts: []Fact{
 			{
-				Name: "AssignsValues",
+				Name: "StringValueMatches",
 				SideEffects: []SideEffect{
-					{FieldName: "Int1", ExpectedValue: value.NewCombined(value.NewInt(-9007199254740993), value.Add, value.NewInt(1))},
-					{FieldName: "Int2", ExpectedValue: value.NewCombined(value.NewInt(9007199254740993), value.Subtract, value.NewInt(1))},
+					{FieldName: "String1", ExpectedValue: value.NewString("-9007199254740993")},
+					{FieldName: "String2", ExpectedValue: value.NewString("9007199254740993")},
 				},
 			},
 		},
